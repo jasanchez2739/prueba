fec4bba36b8d4f5b7d12f3a1a99ce209679949d9
Jorge sanchez <jasanchez2739@gmail.com>
2016-09-07 08:14:04 -0500

    Update ViewController.swift

--- Prueba 1/ViewController.swift
+++ Prueba 1/ViewController.swift
@@ -22,7 +22,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
+for numbers in 0..<101 {
+    var a = numbers
+    var b = numbers % 5
+    var c = numbers % 2
+    var d = numbers > 29 && numbers < 41
+    
+    
+    if
+        d == true {
+        print("\(a) Viva Swift!!!")
+        
+    }else if c == 0{
+        print("\(a) Par!!!")
+        
+    }else if b == 0 {
+        
+        print("\(a) Bingo!!!")
+        
+        
+    }else if c != 0 {
+        print("\(a) impar!!!")
+        
+    }else{ print(a)
+    }
+}
+
 
